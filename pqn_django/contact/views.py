14bf86d0c856416d70b5eb8b94d284a37c9d2732
JHernanSan <jhernan.san@gmail.com>
2023-12-17 15:33:08 -0300

    formulario ya carga en bd

--- pqn_django/contact/views.py
+++ pqn_django/contact/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .forms import ContactForm
 
 # Create your views here.
 
 def contact(request):
     contact_form = ContactForm()
+    
+    if request.method == 'POST':
+        contact_form = ContactForm(data=request.POST)
+        
+        if contact_form.is_valid():
+            contact_form.save()
+            return redirect(reverse('contact')+'?ok')
+        else:
+            return redirect(reverse('contact')+'?error')            
+            
     return render(request,"core/pages/contact.html", {"form":contact_form})
 
--- pqn_django/pqn_django/settings.py
+++ pqn_django/pqn_django/settings.py
@@ -38,10 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'core',
-    'eventos',
+    'eventos',    
     'contact.apps.ContactConfig'    
 ]
+
+#Configuración del crispyforms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
