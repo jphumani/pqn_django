47e886c087f658851111e02f21d6a95227846258
JHernanSan <jhernan.san@gmail.com>
2023-12-17 11:54:46 -0300

    retoque admin previo crispyforms

--- pqn_django/contact/admin.py
+++ pqn_django/contact/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Contact
 
 # Register your models here.
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "created")
+
+admin.site.register(Contact, ContactAdmin)
